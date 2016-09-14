7888dac96bb1aa1d16ae8f0570c2125da0d8532c
kuyapp <kuyapp@users.noreply.github.com>
2016-09-14 19:24:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sleep 10
 service transmission-daemon stop
 mv /var/lib/transmission-daemon/info/settings.json /var/lib/transmission-daemon/info/settings.json.bak
 cp ./settings.json /var/lib/transmission-daemon/info/settings.json
-usermod -a -G debian-transmission root
+chmod 766 /var/www/html
 service transmission-daemon start
 
 sed -i 's/try_files $uri $uri\/ =404;/autoindex on;/' /etc/nginx/sites-enabled/default
