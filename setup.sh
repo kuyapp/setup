b0ee02ec503526f51ae38b0599ee882c1e01c8a5
kuyapp <kuyapp@users.noreply.github.com>
2016-09-14 18:58:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@
 set -e
 
 sleep 10
+service transmission-daemon stop
+
+mv         /var/lib/transmission-daemon/info/settings.json
+
+usermod -a -G debian-transmission root
+
+service transmission-daemon start
