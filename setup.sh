e769c975cb32c991570d7f6585e98e80f71795d4
kuyapp <kuyapp@users.noreply.github.com>
2019-09-17 16:25:53 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 sleep 10
+add-apt-repository -y ppa:transmissionbt/ppa
+apt-get -y update
+apt-get -y install transmission-cli transmission-common transmission-daemon nginx
 service transmission-daemon stop
 mv /var/lib/transmission-daemon/info/settings.json /var/lib/transmission-daemon/info/settings.json.bak
 cp ./settings.json /var/lib/transmission-daemon/info/settings.json
 chown debian-transmission /var/www/html
 service transmission-daemon start
-
 sed -i 's/try_files $uri $uri\/ =404;/autoindex on;/' /etc/nginx/sites-enabled/default
 rm /var/www/html/*
 service nginx restart
